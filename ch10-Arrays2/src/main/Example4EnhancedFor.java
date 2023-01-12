cfa98c61f3f202d6a7763ab0245b41b01427fb21
cmanu <Cosmin-Mihai.Manu@endava.com>
2023-01-12 13:20:45 +0200

    resolved exception ArrayIndexOOBounds

--- ch10-Arrays2/src/main/Example4EnhancedFor.java
+++ ch10-Arrays2/src/main/Example4EnhancedFor.java
@@ -8,19 +8,19 @@ public class Example4EnhancedFor {
         int [][] array1 = new int[3][2];                 //{ {0,0}, {0,0}, {0,0} }
 
         int [][] array2 = new int[3][];
-        array2[1] = new int[]{1};
-        array2[2] = new int[]{44,34,67};
-        array2[3] = new int[]{10, 12};
+        array2[0] = new int[]{1};
+        array2[1] = new int[]{44,34,67};
+        array2[2] = new int[]{10, 12};
 
 
-        /**
+        /*
          * Enhanced for
          * Right side : collection that we are parsing
          * Left side : variable of the type of the VALUES INSIDE that collection
          */
 
-        for(int[] x : a) {       //what contains a? arrays of int => x
-            System.out.println(x.length);
+        for(int[] x : array2) {       //what contains a? arrays of int => x
+            System.out.println("Length " + x.length);
             for(int y : x) {    //what contains x? ints => y
                 System.out.print(y +" ");
             }
