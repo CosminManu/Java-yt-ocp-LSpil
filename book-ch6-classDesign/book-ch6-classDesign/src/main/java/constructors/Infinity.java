84abda97f7cd18d20b619b8bf54ac94016bc995c
cmanu <Cosmin-Mihai.Manu@endava.com>
2023-02-01 18:00:56 +0200

    constructors

---
+++ book-ch6-classDesign/book-ch6-classDesign/src/main/java/constructors/Infinity.java
@@ -0,0 +1,12 @@
+package constructors;
+
+public class Infinity {
+
+//	public Infinity(int i){
+//		this();
+//	}
+//	
+//	public Infinity(){
+//		this(5);
+//	}
+}
---
+++ book-ch6-classDesign/book-ch6-classDesign/src/main/java/constructors/Turtle.java
@@ -0,0 +1,17 @@
+package constructors;
+
+public class Turtle {
+	private String name;
+	
+	public Turtle(){
+		name = "Nae Nicolae";
+	}
+	public Turtle(int age){}
+	public Turtle(Integer age){}
+	public Turtle(long age){}
+	public Turtle(Boolean isFemale){}
+	public Turtle(String name, String... favouriteFoods){}
+	
+	// overloading ctors = overloading methods
+	
+}
