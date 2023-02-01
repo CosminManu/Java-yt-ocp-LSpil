6e91d44c29b24f10c78dd7baa4efb6c477e3b635
cmanu <Cosmin-Mihai.Manu@endava.com>
2023-02-01 19:27:24 +0200

    constructors; this., this(), super., super()

---
+++ book-ch6-classDesign/book-ch6-classDesign/src/main/java/constructors/Zoo.java
@@ -0,0 +1,12 @@
+package constructors;
+
+public class Zoo {
+	public Zoo(){
+		super();		// call to parent ctor in java.lang.Object
+		System.out.println();
+
+//		super();	1st statement	
+	}
+	
+
+}
---
+++ book-ch6-classDesign/book-ch6-classDesign/src/main/java/constructors/equivalentClasses/Donkey2.java
@@ -0,0 +1,10 @@
+package constructors.equivalentClasses;
+
+public class Donkey2 {
+
+	public Donkey2(){
+		super();
+	}
+	// no arg ctor + super()
+	//added by java compiler in .class for every class obj
+}
