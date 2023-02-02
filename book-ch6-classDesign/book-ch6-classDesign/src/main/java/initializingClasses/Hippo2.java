bb23c1beb984d53960073acaeb2efa600b8ef178
cmanu <Cosmin-Mihai.Manu@endava.com>
2023-02-02 21:10:37 +0200

    order for initializing classes

---
+++ book-ch6-classDesign/book-ch6-classDesign/src/main/java/initializingClasses/Hippo2.java
@@ -0,0 +1,16 @@
+package initializingClasses;
+
+public class Hippo2 {
+	public static void main(String[] args) {
+		System.out.print("C");
+		new Hippo();
+		new Hippo();
+		new Hippo();
+		// even though 3 instances,class is LOADED ONLY ONCE 
+
+//		Hippo h = new Hippo();				// un hipopotam este un hipopotam
+//		Animal a = new Hippo();				// un hipopotam este si un animal
+//		Hippo h1 = (Hippo) new Animal();	// un animal (general) NU este un hipopotam neaparat (decat daca ii fac cast implicit)
+	}
+
+}
