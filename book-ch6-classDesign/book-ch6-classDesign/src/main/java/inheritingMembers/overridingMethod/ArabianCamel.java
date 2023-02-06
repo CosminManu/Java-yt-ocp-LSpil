daa36fc781bdf95a8e520b694e813daac2e9fef7
cmanu <Cosmin-Mihai.Manu@endava.com>
2023-02-06 21:05:27 +0200

    added rule #4 for overriding

--- book-ch6-classDesign/book-ch6-classDesign/src/main/java/inheritingMembers/overridingMethod/ArabianCamel.java
+++ book-ch6-classDesign/book-ch6-classDesign/src/main/java/inheritingMembers/overridingMethod/ArabianCamel.java
@@ -2,7 +2,7 @@ package inheritingMembers.overridingMethod;
 
 public class ArabianCamel extends Camel {
 
-	public int getNumberOfHumps() {
+	protected int getNumberOfHumps() {
 		return 20;
 	}
 	// ERR  attempting to assign weaker access privileges; was public not private, package or protected
@@ -13,4 +13,5 @@ Rule#1 overriding: same name + same parameters
 Rule #2 : method in the child class must be at least as accessible as parent class
 
 Rule #3 : checked/unchecked exceptions
+Rule #4 : return type of the overridden method must be covariant with return type of ht inherited method
  */
