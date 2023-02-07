872c2aa7213dad045b802ad9d8fe5d5c008b31c6
cmanu <Cosmin-Mihai.Manu@endava.com>
2023-02-07 21:36:46 +0200

    finalMethods subchapter

---
+++ book-ch6-classDesign/book-ch6-classDesign/src/main/java/inheritingMembers/finalMethods/Bird.java
@@ -0,0 +1,14 @@
+package inheritingMembers.finalMethods;
+
+public class Bird {
+
+	public final boolean hasFeathers(){
+		return true;
+	}
+	
+	public final static void flyAway(){}
+
+	public static void shootsBalloon(){}
+	
+	
+}
