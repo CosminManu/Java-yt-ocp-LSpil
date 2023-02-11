cfae34aff090f6314818b4bc931bf6e5358258df
cmanu <Cosmin-Mihai.Manu@endava.com>
2023-02-11 19:18:33 +0200

    decrementing for loop (--)

--- book-c3-decisions/book-c3-decisions/src/main/java/forLoop/F.java
+++ book-c3-decisions/book-c3-decisions/src/main/java/forLoop/F.java
@@ -6,10 +6,14 @@ public class F {
 
 		int i= 1;
 		for (i = 0; i < 10; i++) {
-			System.out.println(i);
+			System.out.println(i);		// 0 ... 9
 		}
-		System.out.println(i);		
+		System.out.println(i);			// 10
 		// 0, 1, 2,.. 10 inclusiv (first is i++ then i<10)
 
+		System.out.println();
+		for(var counter = 5; counter >= 0; counter--){
+			System.out.print(counter + " ");		// 5 4 3 2 1
+		}
 	}
 }
